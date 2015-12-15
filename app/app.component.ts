9b0f95a41ff05ed8453f64942c8d7a2975e98486
Érick Anthony de Oliveira Carvalho <erick.carvalho@fead.br>
2015-12-15 14:26:23 -0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by erick.carvalho on 15/12/2015.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ app/boot.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by erick.carvalho on 15/12/2015.
+ */
+
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
